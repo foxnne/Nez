b54a981b053aa96a3b0484539cbcd740cc4afd09
foxnne <foxnne@gmail.com>
2019-11-22 00:48:15 -0600

    Redefined how SpriteAtlasPacker creates animations

--- Nez.SpriteAtlasPacker/SpriteAtlasPacker/SpriteAtlasPacker.cs
+++ Nez.SpriteAtlasPacker/SpriteAtlasPacker/SpriteAtlasPacker.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 
 namespace Nez.Tools.Atlases
 {
@@ -30,7 +31,7 @@ namespace Nez.Tools.Atlases
             public bool RecurseSubdirectories;
             public float OriginX = Constants.DefaultOrigin;
             public float OriginY = Constants.DefaultOrigin;
-            public bool DontCreateAnimations = false;
+            public bool CreateAnimations = true;
             public int FrameRate = Constants.DefaultFrameRate;
             public string[] InputPaths;
             public bool OutputLua;
@@ -52,7 +53,7 @@ namespace Nez.Tools.Atlases
                         while ((line = reader.ReadLine()) != null)
                         {
                             if (string.IsNullOrWhiteSpace(line)) continue;
-                            
+
                             //ignore lines with comments
                             if (line.IndexOf("#") > 0) { continue; }
 
@@ -97,9 +98,9 @@ namespace Nez.Tools.Atlases
                                 case "originy":
                                     config.OriginY = float.Parse(value);
                                     break;
-                                case "dontcreateanimations":
-                                    if (value == "true") { config.DontCreateAnimations = true; }
-                                    else config.DontCreateAnimations = false;
+                                case "createanimations":
+                                    if (value == "true") { config.CreateAnimations = true; }
+                                    else config.CreateAnimations = false;
                                     break;
                                 case "framerate":
                                     config.FrameRate = Int32.Parse(value);
@@ -108,7 +109,6 @@ namespace Nez.Tools.Atlases
                                     if (value == "true") { config.OutputLua = true; }
                                     else config.OutputLua = false;
                                     break;
-
                             }
                         }
                     }
@@ -126,7 +126,11 @@ namespace Nez.Tools.Atlases
             // compile a list of images
             var animations = new Dictionary<string, List<string>>();
             var images = new List<string>();
-            FindImages(config, images, animations);
+
+            //FindImages(config, images, animations);
+            int error = FindImages(config, images);
+            if (error != 0) { return error; }
+            
 
             // make sure we found some images
             if (images.Count == 0)
@@ -135,8 +139,11 @@ namespace Nez.Tools.Atlases
                 return (int)FailCode.NoImages;
             }
 
+            if (config.CreateAnimations) error = CreateAnimations(config, images, animations);
+            if (error != 0) { return error; }
+
             // make sure no images have the same name if we're building a map
-            for (int i = 0; i < images.Count; i++)
+           /*  for (int i = 0; i < images.Count; i++)
             {
                 var str1 = Path.GetFileNameWithoutExtension(images[i]);
                 for (int j = i + 1; j < images.Count; j++)
@@ -148,7 +155,7 @@ namespace Nez.Tools.Atlases
                         return (int)FailCode.ImageNameCollision;
                     }
                 }
-            }
+            } */
 
             // generate our output
             var imagePacker = new ImagePacker();
@@ -195,7 +202,60 @@ namespace Nez.Tools.Atlases
             return 0;
         }
 
-        static void FindImages(Config arguments, List<string> images, Dictionary<string, List<string>> animations)
+        static int FindImages(Config arguments, List<string> images)
+        {
+            var fileNames = new List<string>();
+            foreach (var path in arguments.InputPaths)
+            {
+                if (Directory.Exists(path))
+                {
+                    foreach (string file in Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories).Where(file => MiscHelper.IsImageFile(file)))
+                    {
+                        var fileName = Path.GetFileNameWithoutExtension(file);
+                        if (fileNames.Contains(fileName))
+                        {
+                            System.Console.WriteLine("Two images have the same name: {0} = {1}", file, images[fileNames.IndexOf(fileName)]);
+                            images.Clear();
+                            return (int)FailCode.ImageNameCollision;
+                        }
+                        fileNames.Add(fileName);
+                        images.Add(file);
+                    }
+                }
+            }
+            return 0;
+        }
+
+        //creates animations based on the file name of the images retrieved in FindImages
+        //expects animations to be named similar to: "animation_0" , "animation_1", ...
+        //the key element is that all animation frames end in an underscore (_) and then a numeric value
+
+        static int CreateAnimations (Config arguments, List<string> images, Dictionary<string,List<string>> animations)
+        {
+            foreach (var image in images)
+            {
+                var name = Path.GetFileNameWithoutExtension(image);
+
+                //only consider images with digits at the end of the filename
+                if (char.IsDigit(name[name.Length - 1]))
+                {   
+                    var animationName = name.Substring(0, name.LastIndexOf('_'));
+                    if (animations.TryGetValue(animationName, out var animationNames))
+                    {
+                        animationNames.Add(image);
+                    }
+                    else
+                    {
+                        var newAnimation = new List<string>() { image };
+                        animations.Add(animationName, newAnimation);
+                    }
+                }
+            }
+            return 0;
+        }
+
+
+        /* static void FindImages(Config arguments, List<string> images, Dictionary<string, List<string>> animations)
         {
             foreach (var str in arguments.InputPaths)
             {
@@ -208,7 +268,7 @@ namespace Nez.Tools.Atlases
                     }
 
                     foreach (var dir in Directory.GetDirectories(str))
-                        FindImagesRecursively(dir, images, animations, !arguments.DontCreateAnimations);
+                        FindImagesRecursively(dir, images, animations, arguments.CreateAnimations);
                 }
                 else if (MiscHelper.IsImageFile(str))
                 {
@@ -234,6 +294,6 @@ namespace Nez.Tools.Atlases
 
             foreach (var subdir in Directory.GetDirectories(dir))
                 FindImagesRecursively(subdir, images, animations, createAnimations);
-        }
+        } */
     }
 }
